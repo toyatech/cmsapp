e592a2fc1055885835dba2dc9a5de4da56412827
noisypebble <noisypebble@toyatech.net>
2016-04-10 22:45:39 -0500

    simple generation

--- generate.js
+++ generate.js
@@ -1,68 +1,24 @@
-var gulp = require('gulp')
-  , runSequence = require('run-sequence')
-  , tap = require('gulp-tap')
-  , source = require('viynl-source-stream')
-  , rename = require('gulp-rename')
-  , estemplate = require('gulp-estemplate')
-  , path = require('path')
-  , print = require('gulp-print');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const rmdirp = require('rmdirp');
+const _ = require('lodash');
 
-var memory = {
-  templates: {},
-  schemas: {},
-  packages: []
-};
+const templatesDir = '/home/dlittle/Projects/mean-templates/templates/';
+const schemasDir = '/home/dlittle/Projects/cmsapp/schemas/';
 
-var paths = {
-  templates: '../mean-estemplates/**/*.jst',
-  schemas: 'schemas/**/*.json',
-  packages: 'packages/custom'
-};
-
-function loadTemplates() {
-  return gulp.src([paths.templates])
-    .pipe(tap(function(file) {
-      memory.templates[file.path.replace(/^.*\/mean-estemplates\//, '')] = String(file.contents);
-    }));
+const templates = {
+  servers: {
+    models: _.template(
+      fs.readFileSync(templatesDir + 'server/models/model.jst'),
+      { 'imports': require(templatesDir + 'server/models/imports') }
+    )
+  }
 }
 
-function loadSchemas() {
-  return gulp.src([paths.schemas])
-    .pipe(tap(function(file) {
-      memory.schemas[file.path.replace(/^.*\/schemas\//, '')] = String(file.contents);
-    }));
+const schemas = {
+  'cmsapp-contacts': {
+    'Church': require(schemasDir + 'cmsapp-contacts/Church.json')
+  }
 }
 
-function loadPackages() {
-  return gulp.src(['schemas/*'])
-    .pipe(tap(function(file) {
-      memory.packages.push(path.basename(file.path));
-    }));
-};
-
-function generatePackages() {
-  
-  var packages = memory.packages;
-  var templates = Object.keys(memory.templates);
-
-  var streams = [];
-  
-  packages.forEach(function(package) {
-    templates.forEach(function(template) {
-      var stream = source(package + '/' path.parse(template).name + '.js');
-      var streamEnd = stream;
-
-      var fileContents = 
-
-};
-
-exports.loadTemplates = loadTemplates;
-exports.loadSchemas = loadSchemas;
-exports.loadPackages = loadPackages;
-exports.generatePackages = generatePackages;
-
-var generate = gulp.series(gulp.parallel(loadTemplates, loadSchemas, loadPackages), generatePackages);
-
-gulp.task('generate', generate);
-
-gulp.task('default', gulp.series('generate'));
+console.log(templates.servers.models(schemas['cmsapp-contacts'].Church));
